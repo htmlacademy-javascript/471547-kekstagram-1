3b8d46793dc92707d3dcd91285f92186c16062b1
Ekaterina Baidukova <kate.baidukova@gmail.com>
2023-10-07 12:25:35 +0300

    Correcting mistakes after checking homework

--- js/function.js
+++ js/function.js
@@ -1,66 +1,56 @@
 
 /*Задание: функция для проверки, является ли строка палиндромом. Если хотите усложнить задание, предусмотрите случай, когда в строке встречаются пробелы. Они не должны учитываться при проверке!*/
 
-/*let text = 'кубик';
-let chekingText = text.replaceAll(' ', '');
-
-function isPalindrome(string) {
-    let pattern = string.toLowerCase();
-    let check = ('');
-    for (let i = pattern.length - 1; i >= 0; --i) {
-      check += pattern[i];
-    }
-    return pattern == check;
+/*function isPalindrome(string) {
+  const formattedString = string.replaceAll(' ', '').toLowerCase();
+  let reversedString = ('');
+  for (let i = formattedString.length - 1; i >= 0; --i) {
+    reversedString += formattedString[i];
   }
+  return formattedString === reversedString;
+}
 
-  console.log(isPalindrome(chekingText));
-*/
+console.log(isPalindrome('а роза упала на лапу Азора'));*/
 
 /*Задание: функция, которая принимает строку, извлекает содержащиеся в ней цифры от 0 до 9 и возвращает их в виде целого положительного числа. Если в строке нет ни одной цифры, функция должна вернуть NaN. Если хотите усложнить задание, предусмотрите случай, когда вместо строки приходит число.*/
 
-/*let data = ' dfsfffs10 agga 12';
-
-function cheking (text) {
-    if (typeof text === "string") {
-      return newText = parseInt(text.replace(/\D/g,''), 10);
-    }
-    if (typeof text === "number") {
-        notNumber = text.toString();
-        return newText = notNumber.replace(/\D/g,'');
-    }
-    return NaN;
+/*function getNumberFromString (string) {
+  if (typeof string === 'string') {
+    const newString = parseInt(string.replace(/\D/g,''), 10);
+    return newString;
   }
+  if (typeof string === 'number') {
+    const newString = string.toString().replace(/\D/g,'', 10);
+    return newString;
+  }
+  return NaN;
+}
 
-console.log(cheking(data));*/
+console.log(getNumberFromString('sdsd122 4/666.5'));*/
 
 /*Задание: функция, которая принимает три параметра: исходную строку, минимальную длину и строку с добавочными символами — и возвращает исходную строку, дополненную указанными символами до заданной длины. Символы добавляются в начало строки. Если исходная строка превышает заданную длину, она не должна обрезаться. Если «добивка» слишком длинная, она обрезается с конца.*/
 
-/*let string1 = '1';
-let string2 = '0';
-let num = 9;
+/*const string = '1dgdgsg';
+const addedText = '0';
+const lengthString = 19;
 
-function text(string1, num, string2) {
-    if (string1.length < num) {
-        let res = string2.substr(0, num - string1.length) + string1;
-            if (res.length < num) {
-            return string2.repeat(num - string1.length) + string1;
-        };
-        return res;
-    };
-    return string1;
+function addPadString(currentString, length, padString) {
+  if (currentString.length < length) {
+    const res = padString.substr(0, length - currentString.length) + currentString;
+    if (res.length < length) {
+      return padString.repeat(length - currentString.length) + currentString;
+    }
+    return res;
+  }
+  return currentString;
 }
-console.log(text(string1, num, string2));*/
+console.log(addPadString(string, lengthString, addedText));*/
 
 /*Задание для проекта «Кекстаграм»: функция для проверки длины строки. Она принимает строку, которую нужно проверить, и максимальную длину и возвращает true, если строка меньше или равна указанной длине, и false, если строка длиннее.*/
 
-/*let string = 'sss';
-let num = 5;
+const currentString = 'd';
+const stringLength = 5;
 
-function chekingString(data,num) {
-  if (data.length <= num) {
-    return true;
-  }
-  return false;
-}
+const StringLength = (string, length) => string.length <= length;
 
-console.log(chekingString(string,num));*/
+console.log(StringLength(currentString, stringLength));
