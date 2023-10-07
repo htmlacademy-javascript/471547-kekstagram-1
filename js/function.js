6aa5250e59aebfa802bfd8dd8991e0b88191ff54
Ekaterina Baidukova <kate.baidukova@gmail.com>
2023-10-07 19:45:07 +0300

    Убирает переменные перед объявлением функции

--- js/function.js
+++ js/function.js
@@ -10,29 +10,27 @@ function isPalindrome(string) {
   return formattedString === reversedString;
 }
 
+// eslint-disable-next-line no-console
 console.log(isPalindrome('а роза упала на лапу Азора'));
 
 /*Задание: функция, которая принимает строку, извлекает содержащиеся в ней цифры от 0 до 9 и возвращает их в виде целого положительного числа. Если в строке нет ни одной цифры, функция должна вернуть NaN. Если хотите усложнить задание, предусмотрите случай, когда вместо строки приходит число.*/
 
-function getNumberFromString (string) {
-  if (typeof string === 'string') {
-    const newString = parseInt(string.replace(/\D/g,''), 10);
+function getNumberFromString (chekingString) {
+  if (typeof chekingString === 'string') {
+    const newString = parseInt(chekingString.replace(/\D/g,''), 10);
     return newString;
   }
-  if (typeof string === 'number') {
-    const newString = string.toString().replace(/\D/g,'', 10);
+  if (typeof chekingString === 'number') {
+    const newString = chekingString.toString().replace(/\D/g,'', 10);
     return newString;
   }
   return NaN;
 }
 
+// eslint-disable-next-line no-console
 console.log(getNumberFromString('sdsdsfdsf3122 4/666.5'));
 
 /*Задание: функция, которая принимает три параметра: исходную строку, минимальную длину и строку с добавочными символами — и возвращает исходную строку, дополненную указанными символами до заданной длины. Символы добавляются в начало строки. Если исходная строка превышает заданную длину, она не должна обрезаться. Если «добивка» слишком длинная, она обрезается с конца.*/
-
-const string = '1dgsdsddgsg';
-const addedText = '0';
-const lengthString = 19;
 
 function addPadString(currentString, length, padString) {
   if (currentString.length < length) {
@@ -44,13 +42,13 @@ function addPadString(currentString, length, padString) {
   }
   return currentString;
 }
-console.log(addPadString(string, lengthString, addedText));
+
+// eslint-disable-next-line no-console
+console.log(addPadString('1dgsdsddgsg', 0, 19));
 
 /*Задание для проекта «Кекстаграм»: функция для проверки длины строки. Она принимает строку, которую нужно проверить, и максимальную длину и возвращает true, если строка меньше или равна указанной длине, и false, если строка длиннее.*/
 
-const currentString = 'd';
-const stringLength = 5;
-
 const StringLength = (string, length) => string.length <= length;
 
-console.log(StringLength(currentString, stringLength));
+// eslint-disable-next-line no-console
+console.log(StringLength('d', 5));
