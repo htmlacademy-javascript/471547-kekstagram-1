b1c5861ece42e2c6350df2476ff9a549103a3afd
Ekaterina Baidukova <kate.baidukova@gmail.com>
2023-12-14 12:41:35 +0300

    Вносит правки по комментариям

--- js/miniatures.js
+++ js/miniatures.js
@@ -4,15 +4,13 @@ const picturesContainer = document.querySelector('.pictures');
 
 const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
 
-const picturesCollection = images;
-
 const picturesFragment = document.createDocumentFragment();
 
-picturesCollection.forEach(({url, likes, comments}) => {
+images.forEach(({url, likes, comments}) => {
   const pictureElement = pictureTemplate.cloneNode(true);
-  document.querySelector('#picture').content.querySelector('.picture__img').src = url;
-  document.querySelector('#picture').content.querySelector('.picture__likes').textContent = likes;
-  document.querySelector('#picture').content.querySelector('.picture__comments').textContent = comments.length;
+  pictureElement.querySelector('#picture').content.querySelector('.picture__img').src.alt = url.alt;
+  pictureElement.querySelector('#picture').content.querySelector('.picture__likes').textContent = likes;
+  pictureElement.querySelector('#picture').content.querySelector('.picture__comments').textContent = comments.length;
   picturesFragment.append(pictureElement);
 });
 
