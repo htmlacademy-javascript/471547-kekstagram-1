db00720c298f126c7dda09f41e2238ab71b21832
Ekaterina Baidukova <kate.baidukova@gmail.com>
2023-12-14 14:13:54 +0300

    добавляет функцию для создания коллекции картинок

--- js/main.js
+++ js/main.js
@@ -3,6 +3,9 @@ import './util.js';
 import './miniatures.js';
 
 import {images} from './data.js';
+import {getPicturesCollection} from './miniatures.js';
+
+getPicturesCollection(images);
 
 // eslint-disable-next-line no-console
 console.log(images);
--- js/miniatures.js
+++ js/miniatures.js
@@ -1,18 +1,18 @@
-import {images} from './data.js';
-
 const picturesContainer = document.querySelector('.pictures');
 
 const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
 
-const picturesFragment = document.createDocumentFragment();
+const getPicturesCollection = (array) => {
+  const picturesFragment = document.createDocumentFragment();
+  array.forEach(({url, description, likes, comments}) => {
+    const pictureElement = pictureTemplate.cloneNode(true);
+    pictureElement.querySelector('.picture__img').src = url;
+    pictureElement.querySelector('.picture__img').alt = description;
+    pictureElement.querySelector('.picture__likes').textContent = likes;
+    pictureElement.querySelector('.picture__comments').textContent = comments.length;
+    picturesFragment.append(pictureElement);
+  });
+  picturesContainer.append(picturesFragment);
+};
 
-images.forEach(({url, description, likes, comments}) => {
-  const pictureElement = pictureTemplate.cloneNode(true);
-  pictureElement.querySelector('.picture__img').src = url;
-  pictureElement.querySelector('.picture__img').alt = description;
-  pictureElement.querySelector('.picture__likes').textContent = likes;
-  pictureElement.querySelector('.picture__comments').textContent = comments.length;
-  picturesFragment.append(pictureElement);
-});
-
-picturesContainer.append(picturesFragment);
+export {getPicturesCollection};
