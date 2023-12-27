693e3532b2bbedc4aa425da029567d60e1d0021d
Ekaterina Baidukova <kate.baidukova@gmail.com>
2023-12-27 15:13:38 +0300

    исправляет ошибку в выводе комментариев

--- js/big-picture.js
+++ js/big-picture.js
@@ -1,32 +1,22 @@
-
-
 const bigPicture = document.querySelector('.big-picture');
 const commentCount = document.querySelector('.social__comment-count');
 const commentList = document.querySelector('.social__comments');
 const commentsLoader = document.querySelector('.comments-loader');
 const body = document.querySelector('body');
 const cancelButton = document.querySelector('.big-picture__cancel');
+const commentTemplate = document.querySelector('#comment').content.querySelector('.social__comment');
 
-const createComment = ({avatar, name, message}) => {
-  const comment = document.createElement('li');
-  comment.innerHTML =
-  '<img class = "social__picture" src = "" alt = "" width = "35" height = "35">';
-  comment.querySelector('.social__picture').src = avatar;
-  comment.querySelector('.social__picture').alt = name;
-  comment.querySelector('.social__text').textContent = message;
-
-  return comment;
-};
-
-const renderComments = (comments) => {
-
-  const fragment = document.createDocumentFragment();
-  comments.forEach((comment) => {
-    const commentElement = createComment(comment);
-    fragment.append(commentElement);
+const createComments = (comments) => {
+  const commentsFragment = document.createDocumentFragment();
+  comments.forEach(({avatar, name, message}) => {
+    const commentElement = commentTemplate.cloneNode(true);
+    const commentAvatar = commentElement.querySelector('.social__picture');
+    commentAvatar.src = avatar;
+    commentAvatar.alt = name;
+    commentElement.querySelector('.social__text').textContent = message;
+    commentsFragment.append(commentElement);
   });
-
-  commentList.append(fragment);
+  commentList.append(commentsFragment);
 };
 
 const hideBigPicture = () => {
@@ -47,21 +37,22 @@ const onCancelButtonClick = () => {
 };
 
 const renderPictureDetails = ({url, likes, description}) => {
-  bigPicture.querySelector('.big-picture__img img').src = url;
-  bigPicture.querySelector('.big-picture__img img').alt = description;
+  const image = bigPicture.querySelector('.big-picture__img img');
+  image.src = url;
+  image.alt = description;
   bigPicture.querySelector('.likes-count').textContent = likes;
   bigPicture.querySelector('.social__caption').textContent = description;
 };
 
-const showBigPicture = (data) => {
+const showBigPicture = (picture) => {
   bigPicture.classList.remove('hidden');
   body.classList.add('modal-open');
   commentsLoader.classList.add('hidden');
   commentCount.classList.add('hidden');
   document.addEventListener('keydown', onDocumentKeydown);
 
-  renderPictureDetails(data);
-  renderComments(data.comments);
+  renderPictureDetails(picture);
+  createComments(picture.comments);
 };
 
 cancelButton.addEventListener('click', onCancelButtonClick);
