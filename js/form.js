8f548110352dfd071a79baab93f52208baa632dc
Ekaterina Baidukova <kate.baidukova@gmail.com>
2024-01-08 21:32:17 +0300

    Исправляет валидацию хэштегов

--- js/form.js
+++ js/form.js
@@ -1,11 +1,19 @@
 //import {resetScale} from './scale.js';
 //import {resetEffects} from './effects.js';
 
-const MAX_HASHTAG_COUNT = 5;
-const MIN_HASHTAG_LENGTH = 2;
-const MAX_HASHTAG_LENGTH = 20;
+const Hashtag = {
+  MAX_LENGTH: 20,
+  MIN_LENGTH: 2,
+  MAX_COUNT: 5
+};
+
 const VALID_SYMBOLS = /^#[A-Za-zА-Яа-яЁё0-9]{1,19}$/i;
-const TAG_ERROR_TEXT = 'Хэштеги заполнены неверно';
+const TAG_ERROR_TEXT_1 = 'Первым символом должен быть знак #';
+const TAG_ERROR_TEXT_2 = 'Минимальная длина хэштега — 2 символа';
+const TAG_ERROR_TEXT_3 = 'Максимальная длина хэштега — 20 символов';
+const TAG_ERROR_TEXT_4 = 'Использован неверный символ при написании хэштега';
+const TAG_ERROR_TEXT_5 = 'Допустимо использование не более 5 хэштегов';
+const TAG_ERROR_TEXT_6 = 'Такой хэштег уже использован';
 
 const imageUploadForm = document.querySelector('.img-upload__form');
 const imageUploadOverlay = document.querySelector('.img-upload__overlay');
@@ -56,34 +64,75 @@ const onFileInputChange = () => {
   showModal();
 };
 
-const startHashtags = (string) => string[0] === '#';
-
-const hasValidLength = (string) =>
-  string.length >= MIN_HASHTAG_LENGTH && string.length <= MAX_HASHTAG_LENGTH;
-
-const hasValidSymbols = (string) => !VALID_SYMBOLS.test(string.slice(1));
-
-const isValidHashtag = (tag) => startHashtags(tag) && hasValidLength(tag) && hasValidSymbols(tag);
-
-const isValidCountHashtags = (tags) => tags.length <= MAX_HASHTAG_COUNT;
-
-const hasUniqueHashtags = (tags) => {
-  const lowerCaseTags = tags.map((tag) => tag.toLowerCase());
-  return lowerCaseTags.length === new Set(lowerCaseTags).size;
-};
-
-const validateHashgtags = (value) => {
+const getHashtagsFromString = (value) => {
   const tags = value
     .trim()
     .split(' ')
     .filter((tag) => tag.trim().length);
-  return isValidCountHashtags(tags) && hasUniqueHashtags(tags) && tags.every(isValidHashtag);
+  return tags;
+};
+
+const checkFirstSymbolHashtag = (string) => {
+  getHashtagsFromString(string);
+  return string[0] === '#';
+};
+
+const hasValidMinLength = (string) => {
+  getHashtagsFromString(string);
+  return string.length >= Hashtag.MIN_LENGTH;
+};
+
+const hasValidMaxLength = (string) => {
+  getHashtagsFromString(string);
+  return string.length <= Hashtag.MAX_LENGTH;
+};
+
+const hasValidSymbols = (string) => {
+  getHashtagsFromString(string);
+  return !VALID_SYMBOLS.test(string.slice(1));
+};
+
+const checkHashtagsCount = (tags) => getHashtagsFromString(tags).length <= Hashtag.MAX_COUNT;
+
+const hasUniqueHashtags = (tags) => {
+  const lowerCaseTags = getHashtagsFromString(tags).map((tag) => tag.toLowerCase());
+  return lowerCaseTags.length === new Set(lowerCaseTags).size;
 };
 
 pristine.addValidator(
   hashtagField,
-  validateHashgtags,
-  TAG_ERROR_TEXT
+  checkFirstSymbolHashtag,
+  TAG_ERROR_TEXT_1
+);
+
+pristine.addValidator(
+  hashtagField,
+  hasValidMinLength,
+  TAG_ERROR_TEXT_2
+);
+
+pristine.addValidator(
+  hashtagField,
+  hasValidMaxLength,
+  TAG_ERROR_TEXT_3
+);
+
+pristine.addValidator(
+  hashtagField,
+  hasValidSymbols,
+  TAG_ERROR_TEXT_4
+);
+
+pristine.addValidator(
+  hashtagField,
+  checkHashtagsCount,
+  TAG_ERROR_TEXT_5
+);
+
+pristine.addValidator(
+  hashtagField,
+  hasUniqueHashtags,
+  TAG_ERROR_TEXT_6
 );
 
 const onFormSubmit = (evt) => {
